44188425530f84955e08075c40ee8c912834f7bf
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-09-29 13:11:31 +0200

    webapp

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -1,244 +1,224 @@
 <template>
-  <div
-    class="ww-webapp-sidebar"
-    :class="{ editing: isEditing }"
-    :style="cssVariables"
-  >
-    <div class="content" v-if="!isTopOrBottom">
-      <div class="header">
-        <wwElement
-          class="header__content"
-          :class="{ hidden: !isOpen }"
-          v-bind="content.headerContent"
-        ></wwElement>
-        <div @click="toggleSidebar">
-          <wwElement
-            class="header__logo"
-            v-bind="content.headerLogo"
-          ></wwElement>
+    <div class="ww-webapp-sidebar" :class="{ editing: isEditing }" :style="cssVariables">
+        <div v-if="!isTopOrBottom" class="content">
+            <div class="header">
+                <wwElement
+                    class="header__content"
+                    :class="{ hidden: !isOpen }"
+                    v-bind="content.headerContent"
+                ></wwElement>
+                <div @click="toggleSidebar">
+                    <wwElement class="header__logo" v-bind="content.headerLogo"></wwElement>
+                </div>
+            </div>
+            <wwElement class="content__layout" v-bind="content.contentLayout"></wwElement>
+            <wwElement
+                v-if="content.fixedBottomLayout"
+                class="content__layout-bottom"
+                v-bind="content.contentLayoutBottom"
+            ></wwElement>
         </div>
-      </div>
-      <wwElement
-        class="content__layout"
-        v-bind="content.contentLayout"
-      ></wwElement>
-      <wwElement
-        v-if="content.fixedBottomLayout"
-        class="content__layout-bottom"
-        v-bind="content.contentLayoutBottom"
-      ></wwElement>
-    </div>
-    <div class="content-mobile" v-else>
-      <wwElement
-        class="content-mobile__layout"
-        v-bind="content.contentLayout"
-      ></wwElement>
-    </div>
+        <div v-else class="content-mobile">
+            <wwElement class="content-mobile__layout" v-bind="content.contentLayout"></wwElement>
+        </div>
 
-    <!-- wwEditor:start -->
-    <div class="ww-webapp-sidebar__menu">
-      <wwEditorIcon small name="menu" />
+        <!-- wwEditor:start -->
+        <div class="ww-webapp-sidebar__menu">
+            <wwEditorIcon small name="menu" />
+        </div>
+        <!-- wwEditor:end -->
     </div>
-    <!-- wwEditor:end -->
-  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    content: { type: "Object", required: true },
-    /* wwEditor:start */
-    wwEditorState: { type: Object, required: true },
-    /* wwEditor:end */
-  },
-  emits: ["update:content"],
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
-  computed: {
-    isEditing() {
-      /* wwEditor:start */
-      return (
-        this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION
-      );
-      /* wwEditor:end */
-      // eslint-disable-next-line no-unreachable
-      return false;
+    props: {
+        content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
     },
-    isTopOrBottom() {
-      return (
-        this.content.positioning === "top" ||
-        this.content.positioning === "bottom"
-      );
+    emits: ['update:content'],
+    data() {
+        return {
+            isOpen: false,
+        };
     },
-    sidebarWidth() {
-      return this.isOpen === true
-        ? this.content.widthOpen
-        : this.content.widthClosed;
-    },
-    positioningValues() {
-      switch (this.content.positioning) {
-        case "left":
-          return {
-            top: this.content.spacings,
-            left: this.content.spacings,
-            bottom: this.content.spacings,
-            width: this.sidebarWidth,
-          };
-        case "right":
-          return {
-            top: this.content.spacings,
-            right: this.content.spacings,
-            bottom: this.content.spacings,
-            width: this.sidebarWidth,
-          };
-        case "bottom":
-          return {
-            right: this.content.spacings,
-            left: this.content.spacings,
-            bottom: this.content.spacings,
-            height: this.content.heightBottomTop,
-            width: "auto",
-          };
-        case "top":
-          return {
-            right: this.content.spacings,
-            left: this.content.spacings,
-            top: this.content.spacings,
-            height: this.content.heightBottomTop,
-            width: "auto",
-          };
-        default:
-          break;
-      }
-    },
-    cssVariables() {
-      const variables = {
-        ...this.positioningValues,
-        "--widthOpen": this.content.widthOpen,
-        "--headerJustify": this.isOpen === true ? "space-between" : "center",
-        "--transitionDuration": this.isEditing
-          ? "0ms"
-          : this.content.transitionDuration,
-        "--transitionTimingFunction": this.content.transitionTimingFunction,
-        "--shadows": this.content.shadows,
-        "--flexDirection": this.isTopOrBottom ? "row" : "column",
-      };
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        isTopOrBottom() {
+            return this.content.positioning === 'top' || this.content.positioning === 'bottom';
+        },
+        sidebarWidth() {
+            return this.isOpen === true ? this.content.widthOpen : this.content.widthClosed;
+        },
+        positioningValues() {
+            switch (this.content.positioning) {
+                case 'left':
+                    return {
+                        top: this.content.spacings,
+                        left: this.content.spacings,
+                        bottom: this.content.spacings,
+                        width: this.sidebarWidth,
+                    };
+                case 'right':
+                    return {
+                        top: this.content.spacings,
+                        right: this.content.spacings,
+                        bottom: this.content.spacings,
+                        width: this.sidebarWidth,
+                    };
+                case 'bottom':
+                    return {
+                        right: this.content.spacings,
+                        left: this.content.spacings,
+                        bottom: this.content.spacings,
+                        height: this.content.heightBottomTop,
+                        width: 'auto',
+                    };
+                case 'top':
+                    return {
+                        right: this.content.spacings,
+                        left: this.content.spacings,
+                        top: this.content.spacings,
+                        height: this.content.heightBottomTop,
+                        width: 'auto',
+                    };
+                default:
+                    return {
+                        top: this.content.spacings,
+                        left: this.content.spacings,
+                        bottom: this.content.spacings,
+                        width: this.sidebarWidth,
+                    };
+            }
+        },
+        cssVariables() {
+            const variables = {
+                ...this.positioningValues,
+                '--widthOpen': this.content.widthOpen,
+                '--headerJustify': this.isOpen === true ? 'space-between' : 'center',
+                '--transitionDuration': this.isEditing ? '0ms' : this.content.transitionDuration,
+                '--transitionTimingFunction': this.content.transitionTimingFunction,
+                '--shadows': this.content.shadows,
+                '--flexDirection': this.isTopOrBottom ? 'row' : 'column',
+            };
 
-      return variables;
+            return variables;
+        },
     },
-  },
-  methods: {
-    toggleSidebar() {
-      console.log("Toggle !");
-      this.isOpen = !this.isOpen;
+    methods: {
+        toggleSidebar() {
+            console.log('Toggle !');
+            this.isOpen = !this.isOpen;
+        },
     },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
 .ww-webapp-sidebar {
-  position: fixed;
-  background-color: inherit;
-  border-radius: inherit;
-  box-shadow: inherit;
-  transition: all var(--transitionDuration) var(--transitionTimingFunction);
+    position: fixed;
+    background-color: inherit;
+    border-radius: inherit;
+    box-shadow: inherit;
+    transition: all var(--transitionDuration) var(--transitionTimingFunction);
 
-  .content {
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-
-    &__layout {
-      overflow-y: auto;
-      height: 100%;
-      width: var(--widthOpen) !important;
-    }
-
-    &__layout-bottom {
-      overflow: hidden;
-      width: var(--widthOpen) !important;
-    }
-
-    .header {
-      display: flex;
-      flex-direction: row;
-      justify-content: var(--headerJustify);
-      align-items: center;
-
-      &__logo {
-        min-width: 30px;
-        min-height: 30px;
-        transition: all var(--transitionDuration)
-          var(--transitionTimingFunction);
-
-        position: absolute;
-        top: 0;
-        right: 0;
-      }
-
-      &__content {
+    .content {
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
         width: 100%;
-        transition: all var(--transitionDuration)
-          var(--transitionTimingFunction);
-        &.hidden {
-          opacity: 0;
-          transform: translateX(-150%);
-          transition: all calc(var(--transitionDuration) / 1.3)
-            var(--transitionTimingFunction);
-        }
-      }
-    }
-  }
+        height: 100%;
 
-  /* wwEditor:start */
-  &.editing:hover {
-    & > .border {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      border: 1px solid var(--ww-editor-color);
-      pointer-events: none;
-      z-index: 10;
+        &__layout {
+            overflow-y: auto;
+            height: 100%;
+            width: var(--widthOpen) !important;
+        }
+
+        &__layout-bottom {
+            overflow: hidden;
+            width: var(--widthOpen) !important;
+        }
+
+        .header {
+            display: flex;
+            flex-direction: row;
+            justify-content: var(--headerJustify);
+            align-items: center;
+
+            &__logo {
+                min-width: 30px;
+                min-height: 30px;
+                transition: all var(--transitionDuration) var(--transitionTimingFunction);
+
+                position: absolute;
+                top: 0;
+                right: 0;
+            }
+
+            &__content {
+                width: 100%;
+                transition: all var(--transitionDuration) var(--transitionTimingFunction);
+                &.hidden {
+                    opacity: 0;
+                    transform: translateX(-150%);
+                    transition: all calc(var(--transitionDuration) / 1.3) var(--transitionTimingFunction);
+                }
+            }
+        }
     }
-    > .ww-webapp-sidebar__menu {
-      opacity: 1;
-      border: 1px solid var(--ww-editor-color);
-      pointer-events: all;
+
+    /* wwEditor:start */
+    &.editing:hover {
+        & > .border {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border: 1px solid var(--ww-editor-color);
+            pointer-events: none;
+            z-index: 10;
+        }
+        > .ww-webapp-sidebar__menu {
+            opacity: 1;
+            border: 1px solid var(--ww-editor-color);
+            pointer-events: all;
+        }
     }
-  }
-  &__menu {
-    display: flex;
-    position: absolute;
-    top: 0px;
-    left: 5px;
-    transform: translate(-50%, -50%);
-    border-radius: 100%;
-    padding: var(--ww-spacing-01);
-    transition: opacity 0.2s ease;
-    z-index: 101;
-    cursor: pointer;
-    background-color: var(--ww-color-blue-500);
-    color: var(--ww-color-white);
-    justify-content: center;
-    align-items: center;
-    opacity: 0;
-    pointer-events: none;
-    &:after {
-      content: "";
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%) rotate(45deg);
+    &__menu {
+        display: flex;
+        position: absolute;
+        top: 0px;
+        left: 5px;
+        transform: translate(-50%, -50%);
+        border-radius: 100%;
+        padding: var(--ww-spacing-01);
+        transition: opacity 0.2s ease;
+        z-index: 101;
+        cursor: pointer;
+        background-color: var(--ww-color-blue-500);
+        color: var(--ww-color-white);
+        justify-content: center;
+        align-items: center;
+        opacity: 0;
+        pointer-events: none;
+        &:after {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
     }
-  }
-  /* wwEditor:end */
+    /* wwEditor:end */
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,158 +1,158 @@
 export default {
-  "editor": {
-    "label": {
-      "en": "Navigation Sidebar"
-    }
-  },
-  properties: {
-    toggleSidebar: {
-      type: "Button",
-      options: {
-        text: { en: "Toggle sidebar state" },
-        color: "blue",
-        action: "toggleSidebar",
-      },
-      section: 'settings',
-      editorOnly: true
+    editor: {
+        label: {
+            en: 'Navigation Sidebar',
+        },
     },
-    fixedBottomLayout: {
-      type: "OnOff",
-      label: {
-        en: "Fixed bottom layout",
-      },
-      section: 'settings',
-      defaultValue: false
+    properties: {
+        toggleSidebar: {
+            type: 'Button',
+            options: {
+                text: { en: 'Toggle sidebar state' },
+                color: 'blue',
+                action: 'toggleSidebar',
+            },
+            section: 'settings',
+            editorOnly: true,
+        },
+        fixedBottomLayout: {
+            type: 'OnOff',
+            label: {
+                en: 'Fixed bottom layout',
+            },
+            section: 'settings',
+            defaultValue: false,
+        },
+        positioning: {
+            label: {
+                en: 'Positioning',
+                fr: 'Positionnement',
+            },
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { value: 'left', label: { en: 'Left', fr: 'Gauche' } },
+                    { value: 'right', label: { en: 'Right', fr: 'Droite' } },
+                    { value: 'bottom', label: { en: 'Bottom', fr: 'Bas' } },
+                    { value: 'top', label: { en: 'Top', fr: 'Haut' } },
+                ],
+            },
+            responsive: true,
+            defaultValue: 'left',
+        },
+        spacings: {
+            type: 'Length',
+            label: {
+                en: 'Spacings',
+                fr: 'Spacings',
+            },
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px' },
+                    { value: '%', label: '%' },
+                ],
+            },
+            responsive: true,
+            defaultValue: '20px',
+        },
+        widthClosed: {
+            type: 'Length',
+            label: {
+                en: 'Width closed',
+                fr: 'Width closed',
+            },
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px' },
+                    { value: '%', label: '%' },
+                ],
+            },
+            responsive: true,
+            defaultValue: '90px',
+        },
+        widthOpen: {
+            type: 'Length',
+            label: {
+                en: 'Width open',
+                fr: 'Width open',
+            },
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px' },
+                    { value: '%', label: '%' },
+                ],
+            },
+            responsive: true,
+            defaultValue: '250px',
+        },
+        heightBottomTop: {
+            hidden: content => content.positioning !== 'top' || content.positioning !== 'bottom',
+            type: 'Length',
+            label: {
+                en: 'Height',
+                fr: 'Height',
+            },
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px' },
+                    { value: '%', label: '%' },
+                ],
+            },
+            responsive: true,
+            defaultValue: '70px',
+        },
+        transitionDuration: {
+            type: 'Length',
+            label: {
+                en: 'Transition duration',
+                fr: 'Durée de la transition',
+            },
+            options: {
+                unitChoices: [{ value: 'ms', label: 'ms', min: 1, max: 5000 }],
+            },
+            defaultValue: '400ms',
+        },
+        transitionTimingFunction: {
+            label: {
+                en: 'Transition timing function',
+                fr: 'Transition timing function',
+            },
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { value: 'ease', label: { en: 'ease', fr: 'ease' } },
+                    { value: 'ease-in', label: { en: 'ease-in', fr: 'ease-in' } },
+                    { value: 'ease-out', label: { en: 'ease-out', fr: 'ease-out' } },
+                    {
+                        value: 'ease-in-out',
+                        label: { en: 'ease-in-out', fr: 'ease-in-out' },
+                    },
+                    { value: 'linear', label: { en: 'linear', fr: 'linear' } },
+                ],
+            },
+            defaultValue: 'ease',
+        },
+        headerLogo: {
+            hidden: true,
+            defaultValue: wwLib.element('ww-flexbox'),
+        },
+        headerContent: {
+            hidden: true,
+            defaultValue: wwLib.element('ww-flexbox'),
+        },
+        contentLayoutBottom: {
+            hidden: true,
+            defaultValue: wwLib.element({
+                type: 'ww-flexbox',
+                content: { direction: 'column' },
+            }),
+        },
+        contentLayout: {
+            hidden: true,
+            defaultValue: wwLib.element({
+                type: 'ww-flexbox',
+                content: { direction: 'column' },
+            }),
+        },
     },
-    positioning: {
-      label: {
-        en: "Positioning",
-        fr: "Positionnement",
-      },
-      type: "TextSelect",
-      options: {
-        options: [
-          { value: "left", label: { en: "Left", fr: "Gauche" } },
-          { value: "right", label: { en: "Right", fr: "Droite" } },
-          { value: "bottom", label: { en: "Bottom", fr: "Bas" } },
-          { value: "top", label: { en: "Top", fr: "Haut" } },
-        ],
-      },
-      responsive: true,
-      defaultValue: 'left'
-    },
-    spacings: {
-      type: "Length",
-      label: {
-        en: "Spacings",
-        fr: "Spacings",
-      },
-      options: {
-        unitChoices: [
-          { value: "px", label: "px" },
-          { value: "%", label: "%" },
-        ],
-      },
-      responsive: true,
-      defaultValue: '20px'
-    },
-    widthClosed: {
-      type: "Length",
-      label: {
-        en: "Width closed",
-        fr: "Width closed",
-      },
-      options: {
-        unitChoices: [
-          { value: "px", label: "px" },
-          { value: "%", label: "%" },
-        ],
-      },
-      responsive: true,
-      defaultValue: '90px'
-    },
-    widthOpen: {
-      type: "Length",
-      label: {
-        en: "Width open",
-        fr: "Width open",
-      },
-      options: {
-        unitChoices: [
-          { value: "px", label: "px" },
-          { value: "%", label: "%" },
-        ],
-      },
-      responsive: true,
-      defaultValue: '250px'
-    },
-    heightBottomTop: {
-      hidden: content => content.positioning !== "top" || content.positioning !== "bottom",
-      type: "Length",
-      label: {
-        en: "Height",
-        fr: "Height",
-      },
-      options: {
-        unitChoices: [
-          { value: "px", label: "px" },
-          { value: "%", label: "%" },
-        ],
-      },
-      responsive: true,
-      defaultValue: '70px'
-    },
-    transitionDuration: {
-      type: "Length",
-      label: {
-        en: "Transition duration",
-        fr: "Durée de la transition",
-      },
-      options: {
-        unitChoices: [{ value: "ms", label: "ms", min: 1, max: 5000 }],
-      },
-      defaultValue: '400ms'
-    },
-    transitionTimingFunction: {
-      label: {
-        en: "Transition timing function",
-        fr: "Transition timing function",
-      },
-      type: "TextSelect",
-      options: {
-        options: [
-          { value: "ease", label: { en: "ease", fr: "ease" } },
-          { value: "ease-in", label: { en: "ease-in", fr: "ease-in" } },
-          { value: "ease-out", label: { en: "ease-out", fr: "ease-out" } },
-          {
-            value: "ease-in-out",
-            label: { en: "ease-in-out", fr: "ease-in-out" },
-          },
-          { value: "linear", label: { en: "linear", fr: "linear" } },
-        ],
-      },
-      defaultValue: 'ease'
-    },
-    headerLogo: {
-      hidden: true,
-      defaultValue: wwLib.element("ww-flexbox"),
-    },
-    headerContent: {
-      hidden: true,
-      defaultValue: wwLib.element("ww-flexbox"),
-    },
-    contentLayoutBottom: {
-      hidden: true,
-      defaultValue: wwLib.element({
-        type: "ww-flexbox",
-        content: { direction: "column" },
-      })
-    },
-    contentLayout: {
-      hidden: true,
-      defaultValue: wwLib.element({
-        type: "ww-flexbox",
-        content: { direction: "column" },
-      })
-    }
-  }
-}
+};
